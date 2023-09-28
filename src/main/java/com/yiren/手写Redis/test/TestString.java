f1e057fa4a25abb070a0b569b03ef48857906a46
weilin <weilin@ufan.cn>
2023-09-28 17:33:22 +0800

    不知道为什么。。。

---
+++ src/main/java/com/yiren/手写Redis/test/TestString.java
@@ -0,0 +1,26 @@
+package com.yiren.手写Redis.test;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.lang.reflect.Field;
+
+/**
+ * <p>
+ * desc:
+ * </p>
+ *
+ * @author weilin
+ * @since 6/9/2023
+ */
+public class TestString {
+
+    public static void main(String[] args) throws Exception{
+        String s = new String("1234567890");
+        String printable = ClassLayout.parseInstance(s).toPrintable();
+        System.out.println(printable);
+        Field value = String.class.getDeclaredField("value");
+        value.setAccessible(true);
+        String printable1 = ClassLayout.parseInstance(value).toPrintable();
+        System.out.println(printable1);
+    }
+}
