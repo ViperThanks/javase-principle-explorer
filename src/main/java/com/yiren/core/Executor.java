49dd4e842e6ea6047cbeea0e0c5d32a4e59817be
Viperthanks <weilin@ufan.cn>
2024-04-13 15:11:56 +0800

    feat：executor加上时间

--- src/main/java/com/yiren/core/Executor.java
+++ src/main/java/com/yiren/core/Executor.java
@@ -1,6 +1,8 @@
 package com.yiren.core;
 
 
+import com.google.common.base.Stopwatch;
+
 /**
  * desc    : 执行者类，封装了一些执行方法
  * 用反射去实现的，就是不想每次都去new一个对象
@@ -55,6 +57,66 @@ public final class Executor {
       }
       LOGGER.info("{} execute explorerClass: [{}] main thread end because {} {}", BEGIN_SIGN, clazzName,
           success ? "execution completed" : "upper exception", END_SIGN);
+    }
+  }
+
+  @SafeVarargs
+  public static void executeExplorerWithTime(Class<? extends Explorer>... explorerClass) {
+    for (Class<? extends Explorer> explorerClazz : explorerClass) {
+      if (explorerClazz == null) {
+        LOGGER.error("explorer is null");
+        continue;
+      }
+      String clazzName = explorerClazz.getSimpleName();
+      LOGGER.info("{} execute explorerClass: [{}] main thread start {}", BEGIN_SIGN, clazzName, END_SIGN);
+      Explorer explorer = null;
+      try {
+        explorer = explorerClazz.getConstructor().newInstance();
+      } catch (Throwable e) {
+        LOGGER.error("{} execute explorerClass: [{}] error {}", BEGIN_SIGN, clazzName, END_SIGN);
+        LOGGER.error("ensure that the explorerClass has a no-args constructor");
+        LOGGER.error("ensure that the explorerClass constructor is public");
+        continue;
+      }
+      boolean success = true;
+      Stopwatch started = Stopwatch.createStarted();
+      try {
+        explorer.explore();
+      } catch (Throwable e) {
+        LOGGER.error("catch inner exception", e);
+        success = false;
+      }
+      Stopwatch stop = started.stop();
+      LOGGER.info("{} execute explorerClass: [{}] main thread end because {} {}", BEGIN_SIGN, clazzName,
+              success ? "execution completed" : "upper exception", END_SIGN);
+      LOGGER.info("{} execute explorerClass: [{}] used {} {}", BEGIN_SIGN, clazzName, stop, END_SIGN);
+    }
+  }
+
+  /**
+   * 基于反射获取运行时class然后再反射调用
+   */
+  @SuppressWarnings("unchecked")
+  public static void executeMyselfWithTime() {
+    try {
+      // 获取当前线程的栈跟踪元素
+      StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+      // stackTraceElements[2] 是调用 doMyself 的方法
+      StackTraceElement caller = stackTraceElements[2];
+      String className = caller.getClassName();
+
+      // 加载类并创建实例
+      Class<?> clazz = Class.forName(className);
+
+      // 检查是否存在 explore 方法并调用
+      if (Explorer.class.isAssignableFrom(clazz)) {
+        executeExplorerWithTime((Class<? extends Explorer>) clazz);
+      }else {
+        throw new IllegalStateException("illegal class because no implement Explorer");
+      }
+    } catch (Exception e) {
+      LOGGER.error("Error in doMyself method", e);
     }
   }
 
